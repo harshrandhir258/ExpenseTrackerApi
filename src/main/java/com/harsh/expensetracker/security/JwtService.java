807ca78fdfb773838be512ee2ae6eafd6235996d
harshrandhir258 <harshrandhir258@gmail.com>
2026-06-03 07:23:58 +0530

    Implemented JWT service and authentication setup

--- src/main/java/com/harsh/expensetracker/security/JwtService.java
+++ src/main/java/com/harsh/expensetracker/security/JwtService.java
@@ -2,6 +2,13 @@ package com.harsh.expensetracker.security;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import io.jsonwebtoken.Jwts;
+import java.util.Date;
+import io.jsonwebtoken.Claims;
+
 
 @Service
 public class JwtService {
@@ -11,4 +18,50 @@ public class JwtService {
 
     @Value("${jwt.expiration}")
     private long jwtExpiration;
+
+    private SecretKey getSignInKey() {
+        return Keys.hmacShaKeyFor(
+                secretKey.getBytes(StandardCharsets.UTF_8)
+        );
+    }
+
+    public String generateToken(String email) {
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .signWith(getSignInKey())
+                .compact();
+    }
+
+    private Claims extractAllClaims(String token) {
+
+        return Jwts
+                .parser()
+                .verifyWith(getSignInKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public String extractUsername(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+
+    public Date extractExpiration(String token) {
+        return extractAllClaims(token).getExpiration();
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public boolean isTokenValid(String token, String email) {
+
+        String username = extractUsername(token);
+
+        return username.equals(email)
+                && !isTokenExpired(token);
+    }
 }
