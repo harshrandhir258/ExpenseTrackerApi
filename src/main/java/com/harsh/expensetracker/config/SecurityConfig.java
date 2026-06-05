caf9edd35b57af1bbc4e60b5b9b2ed1a3af390a4
harshrandhir258 <harshrandhir258@gmail.com>
2026-06-05 07:32:30 +0530

    Implemented JWT authentication and security configuration

--- src/main/java/com/harsh/expensetracker/config/SecurityConfig.java
+++ src/main/java/com/harsh/expensetracker/config/SecurityConfig.java
@@ -1,12 +1,20 @@
 package com.harsh.expensetracker.config;
 
+import com.harsh.expensetracker.security.JwtAuthenticationFilter;
+import com.harsh.expensetracker.service.CustomUserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,6 +27,10 @@ public class SecurityConfig {
                         .permitAll()
                         .anyRequest()
                         .authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 )
 
                 .build();
---
+++ src/main/java/com/harsh/expensetracker/security/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.harsh.expensetracker.security;
+
+import com.harsh.expensetracker.service.CustomUserDetailsService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    private final JwtService jwtService;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("JWT Filter Running");
+        String authHeader = request.getHeader("Authorization");
+
+        String jwtToken = null;
+        String username = null;
+
+        if (authHeader != null &&
+                authHeader.startsWith("Bearer ")) {
+
+            jwtToken = authHeader.substring(7);
+
+            username = jwtService.extractUsername(jwtToken);
+
+            System.out.println("Username = " + username);
+        }
+
+        if (username != null &&
+                SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(username);
+
+            if (jwtService.isTokenValid(
+                    jwtToken,
+                    userDetails.getUsername())) {
+
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
